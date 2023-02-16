569c097053bd3115b8bb94281e31e0c601962cf5
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-02-16 17:58:10 -0500

    finished up routing and page functionality

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const app = express();
+const port = 3001;
+
+const notes = require("./routes/notes.js")
+const api = require("./routes/apinotes.js")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+app.get("/", (req, res ) => {
+    res.render("index.html")
+})
+
+app.use("/notes", notes)
+app.use("/api", api )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port);
+
+
+
+
