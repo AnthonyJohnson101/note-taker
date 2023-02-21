b677259378fd5eb3cfed403e27d1063c89eeae7d
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-02-21 17:59:22 -0500

    deploy fix 74

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const notes = require("./routes/notes.js")
-const api = require("./routes/apinotes.js")
+const notes = require("./routes/notes.js");
+const api = require("./routes/apinotes.js");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res ) => {
     res.render("index.html")
-})
+});
 
-app.use("/notes", notes)
-app.use("/api", api )
+app.use("/notes", notes);
+app.use("/api", api );
 
-
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 
